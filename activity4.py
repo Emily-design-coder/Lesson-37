af2f5d474194b82348de646720665fef6f58d283
Emily-design-coder <emilyrobb2012@icloud.com>
2026-03-11 17:44:04 +1100

    intial commit

---
+++ activity4.py
@@ -0,0 +1,8 @@
+import datetime
+current_time = datetime.datetime.now()
+
+print("Time now at greewhich meridian is: ", end="")
+print(current_time)
+
+import calendar
+print("/n", calendar.calendar(2026))
